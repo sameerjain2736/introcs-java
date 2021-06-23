6e419212809c4a07baf870e48c4e664a3c70ac9a
Sameer Jain <15492377+sameerjain36@users.noreply.github.com>
2021-06-23 10:29:33 -0400

    Added RectangularMatrixDotProduct.java

---
+++ 03-Arrays/RectangularMatrixDotProduct.java
@@ -0,0 +1,46 @@
+public class RectangularMatrixDotProduct {
+    public static void main (String[] args) {
+        int m = Integer.parseInt(args[0]);
+        int n = Integer.parseInt(args[1]);
+        int p = Integer.parseInt(args[2]);
+        int q = Integer.parseInt(args[3]);
+        
+        if (n != p){ 
+            throw new ArithmeticException ("# of cols in Matrix 1 should equal # of rows in Matrix 2");
+        }
+
+        int[][] a = new int[m][n];
+        int[][] b = new int[n][q];
+        int[][] c = new int[m][q];
+
+
+        System.out.println("\nMatrix A");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = (int) (Math.random() * 10);
+                System.out.print(a[i][j] + ", ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nMatrix B");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < q; j++) {
+                b[i][j] = (int) (Math.random() * 10);
+                System.out.print(b[i][j] + ", ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nMatrix C");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < q; j++) {
+                for (int k = 0; k < n; k++) {
+                    c[i][j] += a[i][k]*b[k][j];
+                }
+                System.out.print(c[i][j] + ", ");
+            }
+            System.out.println();
+        }
+    }
+}
