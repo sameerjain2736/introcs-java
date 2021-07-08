71e24bbb483cc3120d188eb07ee6217bcb795ace
Sameer Jain <15492377+sameerjain36@users.noreply.github.com>
2021-07-08 14:56:09 -0400

    Completed ShannonEntropy.java
    Included input file loaded-die.txt to test ShannonEntropy

---
+++ 04-Input-Output/ShannonEntropy.java
@@ -0,0 +1,26 @@
+public class ShannonEntropy {
+    public static void main (String[] args) {
+        int m = Integer.parseInt(args[0]);
+        double entropy = 0.0;
+        int[] countArr = new int[m];
+        int[] inputArr = StdIn.readAllInts();
+
+        for (int i = 1; i <= m; i++) {
+            for (int x : inputArr) {
+                if (i == x)
+                    countArr[i - 1]++;
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            double p = (double)countArr[i - 1]/inputArr.length;
+            if(p == 0)
+                entropy += 0;
+            else
+                entropy += p * Math.log(p)/Math.log(2);
+        }
+
+        entropy *= -1;
+        System.out.printf("%.4f\n", entropy);
+    }
+}
