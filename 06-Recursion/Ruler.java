078ba1f925ecf924f20e0345650a5f9e352d6f42
Sameer Jain <15492377+sameerjain36@users.noreply.github.com>
2021-07-21 09:36:30 -0400

    Create Ruler.java

---
+++ 06-Recursion/Ruler.java
@@ -0,0 +1,12 @@
+public class Ruler {
+    public static String ruler(int n) {
+        if (n == 0) return " ";
+        else
+            return ruler(n-1) + n + ruler(n-1);
+    }
+
+    public static void main (String[] args) {
+        int x = Integer.parseInt(args[0]);
+        System.out.println(ruler(x));
+    }
+}
