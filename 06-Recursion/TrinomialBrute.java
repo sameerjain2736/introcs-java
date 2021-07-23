387435e599798cfbd623b63da930318804252b1f
Sameer Jain <15492377+sameerjain36@users.noreply.github.com>
2021-07-23 11:21:34 -0400

    Update TrinomialBrute.java
    Clarified formatting

--- 06-Recursion/TrinomialBrute.java
+++ 06-Recursion/TrinomialBrute.java
@@ -4,7 +4,9 @@ public class TrinomialBrute {
         if (n == 0 && k == 0) return 1;
         else if (k < -n || k > n) return 0;
         else 
-            return trinomial(n - 1, k - 1) + trinomial(n - 1, k) + trinomial(n - 1, k + 1);
+            return (trinomial(n - 1, k - 1) + 
+                    trinomial(n - 1, k) + 
+                    trinomial(n - 1, k + 1));
     }
 
     // Takes two integer command-line arguments n and k 
