2893029bfac7a4cfdf0eaac45ff21ab077004b0a
Sameer Jain <15492377+sameerjain36@users.noreply.github.com>
2021-08-27 12:06:37 -0400

    Update Huntingtons.java
    Implemented removeWhitespace

--- 08-Abstract-Data-Types/Huntingtons.java
+++ 08-Abstract-Data-Types/Huntingtons.java
@@ -1,16 +1,30 @@
 public class Huntingtons {
 
     // Returns the maximum number of consecutive repeats of CAG in the DNA string.
-    public static int maxRepeats(String dna)
+    public static int maxRepeats(String dna) {
+        return 0;
+    }
 
     // Returns a copy of s, with all whitespace (spaces, tabs, and newlines) removed.
-    public static String removeWhitespace(String s)
+    public static String removeWhitespace(String s) {
+        s = s.replace("\n", "");
+        s = s.replace("\t", "");
+        s = s.replace(" ", "");
+        return s;
+    }
 
     // Returns one of these diagnoses corresponding to the maximum number of repeats:
     // "not human", "normal", "high risk", or "Huntington's".
-    public static String diagnose(int maxRepeats)
+    public static String diagnose(int maxRepeats) {
+        return "hello world";
+    }
 
     // Sample client (see below).
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        In input = new In(args[0]);
+        String s = input.readAll();
+        s = removeWhitespace(s);
+        System.out.print(s);
+    }
 
 }
