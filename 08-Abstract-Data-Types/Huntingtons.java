c9f95668b83a04cb18463b73d353d1af0d8b115a
Sameer Jain <15492377+sameerjain36@users.noreply.github.com>
2021-08-27 14:41:24 -0400

    Update Huntingtons.java
    Finished and tested solution to Huntington's problem

--- 08-Abstract-Data-Types/Huntingtons.java
+++ 08-Abstract-Data-Types/Huntingtons.java
@@ -2,7 +2,23 @@ public class Huntingtons {
 
     // Returns the maximum number of consecutive repeats of CAG in the DNA string.
     public static int maxRepeats(String dna) {
-        return 0;
+        int i = 0;
+        int maxCount = 0;
+        
+        if (dna.length() < 3) return 0;
+
+        while (i < dna.length() - 3) {
+            if (dna.substring(i, i+3).equals("CAG")) {
+                int curCount = 0;
+                while (dna.substring(i, i+3).equals("CAG")) {
+                    curCount++;
+                    i += 3;
+                }
+                if (curCount > maxCount) maxCount = curCount; 
+            }
+            else i++;
+        }
+        return maxCount;
     }
 
     // Returns a copy of s, with all whitespace (spaces, tabs, and newlines) removed.
@@ -16,7 +32,11 @@ public class Huntingtons {
     // Returns one of these diagnoses corresponding to the maximum number of repeats:
     // "not human", "normal", "high risk", or "Huntington's".
     public static String diagnose(int maxRepeats) {
-        return "hello world";
+        if (maxRepeats <= 9 || maxRepeats >= 181) return "not numan";
+        else if (maxRepeats >= 10 && maxRepeats <= 35) return "normal";
+        else if (maxRepeats >= 36 && maxRepeats <= 39) return "high risk";
+        else return "Huntington's";
+
     }
 
     // Sample client (see below).
@@ -24,7 +44,9 @@ public class Huntingtons {
         In input = new In(args[0]);
         String s = input.readAll();
         s = removeWhitespace(s);
-        System.out.print(s);
+        int repeats = maxRepeats(s);
+        System.out.printf("max repeats = %d\n", repeats);
+        System.out.printf(diagnose(repeats));
     }
 
 }
