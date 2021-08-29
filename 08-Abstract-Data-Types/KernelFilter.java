91dd2e5982eb2dd90e06564a986020559001c1df
Sameer Jain <15492377+sameerjain36@users.noreply.github.com>
2021-08-29 18:58:14 -0400

    Update KernelFilter.java
    Tested Picture and Color API with baseline features. Generated and saved a darker version of input PNG file.

--- 08-Abstract-Data-Types/KernelFilter.java
+++ 08-Abstract-Data-Types/KernelFilter.java
@@ -1,24 +1,48 @@
+import java.awt.Color;
+
 public class KernelFilter {
 
     // Returns a new picture that applies the identity filter to the given picture.
-    public static Picture identity(Picture picture)
+    public static Picture identity(Picture picture) {
+        return picture;
+    }
 
     // Returns a new picture that applies a Gaussian blur filter to the given picture.
-    public static Picture gaussian(Picture picture)
+    public static Picture gaussian(Picture picture) {
+        return picture;
+    }
 
     // Returns a new picture that applies a sharpen filter to the given picture.
-    public static Picture sharpen(Picture picture)
+    public static Picture sharpen(Picture picture) {
+        return picture;
+    }
 
     // Returns a new picture that applies an Laplacian filter to the given picture.
-    public static Picture laplacian(Picture picture)
+    public static Picture laplacian(Picture picture) {
+        return picture;
+    }
 
     // Returns a new picture that applies an emboss filter to the given picture.
-    public static Picture emboss(Picture picture)
+    public static Picture emboss(Picture picture) {
+        return picture;
+    }
 
     // Returns a new picture that applies a motion blur filter to the given picture.
-    public static Picture motionBlur(Picture picture)
+    public static Picture motionBlur(Picture picture) {
+        return picture;
+    }
 
     // Test client (ungraded).
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        Picture picture = new Picture(args[0]);
+
+        for (int col = 0; col < picture.width(); col++) {
+            for (int row = 0; row < picture.height(); row++) {
+                Color color = picture.get(col, row);
+                picture.set(col, row, color.darker());
+            }
+        }
+        picture.save("darker.png");
+    }
 
 }
