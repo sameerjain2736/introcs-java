8f7f59d85a862ea332f48df4b2facaa4de215809
Sameer Jain <15492377+sameerjain36@users.noreply.github.com>
2021-08-30 16:37:27 -0400

    Update KernelFilter.java
    Complete implementation of solution before submission

--- 08-Abstract-Data-Types/KernelFilter.java
+++ 08-Abstract-Data-Types/KernelFilter.java
@@ -9,7 +9,7 @@ public class KernelFilter {
             {0, 1, 0},
             {0, 0, 0}
         };
-        return picture;
+        return kernel(picture, weights);
     }
 
     // Returns a new picture that applies a Gaussian blur filter to the given picture.
@@ -19,7 +19,7 @@ public class KernelFilter {
             {.1250, .2500, .1250},
             {.0625, .1250, .0625}
         };
-        return picture;
+        return kernel(picture, weights);
     }
 
     // Returns a new picture that applies a sharpen filter to the given picture.
@@ -29,7 +29,7 @@ public class KernelFilter {
             {-1,  5, -1},
             { 0, -1,  0}
         };
-        return picture;
+        return kernel(picture, weights);
     }
 
     // Returns a new picture that applies an Laplacian filter to the given picture.
@@ -39,7 +39,7 @@ public class KernelFilter {
             {-1,  8, -1},
             {-1, -1, -1}
         };
-        return picture;
+        return kernel(picture, weights);
     }
 
     // Returns a new picture that applies an emboss filter to the given picture.
@@ -49,41 +49,85 @@ public class KernelFilter {
             {-1,  1, 1},
             { 0,  1, 2}
         };
-        return picture;
+        return kernel(picture, weights);
     }
 
     // Returns a new picture that applies a motion blur filter to the given picture.
     public static Picture motionBlur(Picture picture) {
         double[][] weights = {
-            {1/9, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 1/9, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 1/9, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 1/9, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 1/9, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 1/9, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 1/9, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 1/9, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 1/9}
+            {1, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1}
         };
+
+        for (int i = 0; i < weights[0].length; i++) {
+            for (int j = 0; j < weights.length; j++) {
+                weights[i][j] = weights[i][j]/9;
+            }
+        }
         
-        return picture;
+        return kernel(picture, weights);
     }
 
     // Returns a new picture that applies an arbitrary kernel filter to the given picture.
     private static Picture kernel(Picture picture, double[][] weights) {
-        return picture;
+        Picture newPic = new Picture(picture.width(), picture.height());
+        int n          = weights.length;
+        int m          = weights[0].length;
+
+        for (int row = 0; row < picture.height(); row++) {
+            for (int col = 0; col < picture.width(); col++) {
+                int x0 = row - m/2;
+                int y0 = col - n/2;
+
+                double redTotal   = 0.0;
+                double greenTotal = 0.0;
+                double blueTotal  = 0.0;
+
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
+                        greenTotal += weights[i][j] * picture.get(Math.floorMod(j + y0, picture.width()), Math.floorMod(i + x0, picture.height())).getGreen();
+                        redTotal   += weights[i][j] * picture.get(Math.floorMod(j + y0, picture.width()), Math.floorMod(i + x0, picture.height())).getRed();
+                        blueTotal  += weights[i][j] * picture.get(Math.floorMod(j + y0, picture.width()), Math.floorMod(i + x0, picture.height())).getBlue();
+                    }
+                }
+
+                if (redTotal < 0) redTotal = 0;
+                if (redTotal > 255) redTotal = 255;
+                if (greenTotal < 0) greenTotal = 0;
+                if (greenTotal > 255) greenTotal = 255;
+                if (blueTotal < 0) blueTotal = 0;
+                if (blueTotal > 255) blueTotal = 255;
+
+                Color output = new Color((int) redTotal, (int) greenTotal, (int) blueTotal);
+                newPic.set(col, row, output);
+            }
+        }
+        return newPic;
     }
 
     // Test client (ungraded).
     public static void main(String[] args) {
         Picture picture = new Picture(args[0]);
 
-        for (int col = 0; col < picture.width(); col++) {
-            for (int row = 0; row < picture.height(); row++) {
-                Color color = picture.get(col, row);
-                picture.set(col, row, color.darker());
-            }
-        }
-        picture.save("darker.png");
+        Picture identity   = identity(picture);
+        Picture gaussian   = gaussian(picture);
+        Picture sharpen    = sharpen(picture);
+        Picture laplacian  = laplacian(picture);
+        Picture emboss     = emboss(picture);
+        Picture motionBlur = motionBlur(picture);
+
+        identity.save("identity.png");
+        gaussian.save("gaussian.png");
+        sharpen.save("sharpen.png");
+        laplacian.save("laplacian.png");
+        emboss.save("emboss.png");
+        motionBlur.save("motionBlur.png");
     }
 }
